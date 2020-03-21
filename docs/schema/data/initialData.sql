ad75d9961fc7d39588fc552ef27cda51b7b271c4
cristianprochnow <cristianprochnow@gmail.com>
2020-03-21 11:13:28 -0300

    docs(database): creation of initial data that will fill the database
    Just a simple data to fill the database with test informations. Turning possible the proof of
    general structure.

---
+++ docs/schema/data/initialData.sql
@@ -0,0 +1,131 @@
+#===================================================== USER DATA ====================================================#
+
+
+
+#----------------------------------------------------- user TABLE ---------------------------------------------------#
+
+INSERT INTO 
+	`user` (`user_email`, `user_passwd`, `user_name`, `user_phone_num`, `user_address`, `user_bio`, `user_photo`)
+VALUES
+	(
+		'tony@gmail.com',
+        'ddc5f5e86d2f85e1b1ff763aff13ce0a',
+        'Tony Estôncio',
+        '(47) 9 9999-8548',
+        '{"street": "R. Santo Antônio", "city": "Joinville", "state": "Minas Gerais", "country": "Brasil"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'dfhjkdbshjcdjhcbsdj.jpg'
+	),
+    (
+		'silvia.constoun@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Silvia Constoun',
+        '(47) 9 9754-9178',
+        '{"street": "R. Santo José", "city": "Camboriú", "state": "Bahia", "country": "Estados Unidos"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'dashdjuioaskjasbhd.jpg'
+	),
+    (
+		'carlos@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Carlos José',
+        '(47) 9 5465-1564',
+        '{"street": "R. Santo Carlos", "city": "Francesco", "state": "Otawa", "country": "Acre"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'fuiewhwefjkhdjk.jpg'
+	),
+    (
+		'catalognis@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Catalognis Sholson',
+        '(47) 9 1655-5146',
+        '{"street": "R. Santo Luis", "city": "Catacumes", "state": "Acre", "country": "Paralelepípido"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'hfkdjsdjkhfhsdjk.jpg'
+	),
+    (
+		'nomis@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Nomis Solvis',
+        '(47) 9 4324-3422',
+        '{"street": "R. Santo Altonmio", "city": "Araquari", "state": "Texas", "country": "Canadá"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'jhsdkjfhdsjkhfkjsdfjksdhfk.jpg'
+	),
+    (
+		'tomas@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Tomas Aquino',
+        '(47) 9 4235-2445',
+        '{"street": "R. Santo Maurício", "city": "Sintomas", "state": "Pará", "country": "Canudos"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'sadasjfjsdfjsdkf.jpg'
+	),
+    (
+		'sara@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Sara Saragonis',
+        '(47) 9 5476-9785',
+        '{"street": "R. Santo Catomis", "city": "Criciúma", "state": "Paronis", "country": "Havaí"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'dshnfjoisdojfosedof.jpg'
+	),
+    (
+		'solmis@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Solmis Sorilson',
+        '(47) 9 4646-8793',
+        '{"street": "R. Santo Augusto", "city": "Rivolson", "state": "Paraná", "country": "Estado Unidos"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'fewifjhuiwehfiwefwenjfikwef.jpg'
+	),
+    (
+		'lanson@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Lanson Miornis',
+        '(47) 9 5465-8746',
+        '{"street": "R. Santo Caminos", "city": "Dispitopia", "state": "Washington", "country": "Lusana"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'dshfiushfifiushnfiudsduifhsfsdf.jpg'
+	),
+    (
+		'parilson@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Parilson Geminis',
+        '(47) 9 6465-4567',
+        '{"street": "R. Santo Auris", "city": "Florianópolis", "state": "Acre", "country": "Paralelepípido"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'reuighertuiergiuherguiherig.jpg'
+	),
+    (
+		'gilson@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Gilson Mironis',
+        '(47) 9 6546-8614',
+        '{"street": "R. Santo Almiro", "city": "Piratuba", "state": "Espírito Santo", "country": "Suíça"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'vnhfidjkfbnvjkdfnvkjfd.jpg'
+	),
+    (
+		'lusitano@gmail.com',
+        'e5cb7c411f1d9a67f68deff4a954cfbc',
+        'Lusitano Almires',
+        '(47) 9 6546-7763',
+        '{"street": "R. Santo Solwen", "city": "Floresta", "state": "Rio Grande do Sul", "country": "Islândia"}',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec mauris felis. Sed lobortis ante non consequat sagittis. Nullam eu interdum risus. Sed vulputate elementum massa, ut porta nisl feugiat nec. Phasellus dapibus ullamcorper nulla nec sagittis. Suspendisse potenti. Quisque fermentum in libero a egestas. Nullam aliquet odio in hendrerit eleifend. Phasellus venenatis pellentesque aliquam. Vivamus interdum ex nec augue tristique, tristique auctor leo tempor. Aliquam et purus eget justo suscipit ornare nec nec risus. Curabitur urna est, convallis nec fringilla sed, ultricies eget dolor.',
+        'saifuhnsduvisdfnvdfsrv.jpg'
+	);
+
+#-------------------------------------------------- user_type TABLE ---------------------------------------------------#
+
+INSERT INTO 
+	`user_type` (`user_type_desc`) 
+VALUES 
+	('ADM'), ('HOST'), ('GUEST');
+    
+#----------------------------------------------- user__user_type TABLE -------------------------------------------------#
+    
+INSERT INTO 
+	`user__user_type` (`user_user_id`, `user_type_user_type_id`) 
+VALUES
+	(1, 2), (2, 2), (3, 2), (4, 3), (5, 3), (6, 3), (7, 3), (8, 3), (9, 3), (10, 3), (11, 3), (12, 3);
