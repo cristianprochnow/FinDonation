0ba1673480e9a02ce63f212992d0d2204ed8857a
cristianprochnow <cristianprochnow@gmail.com>
2020-09-02 22:13:29 -0300

    feat(Donations): add header into page of donations

--- web/src/pages/Donations/index.tsx
+++ web/src/pages/Donations/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
 
+import {
+  Container,
+  Header,
+  BackLink,
+  Logo,
+  ButtonWithIcon
+} from './styles'
+
+import {
+  RiArrowLeftLine,
+  RiAddCircleLine
+} from 'react-icons/ri'
+
+import logo from '../../assets/images/logos/findonation-with-label.svg'
+
 const Donations: React.FC = () => {
-  return <h1>Hello, Donations!</h1>
+  return (
+    <Container>
+      <Header>
+        <BackLink to="/">
+          <RiArrowLeftLine size={24} />
+          Voltar à página anterior
+        </BackLink>
+
+        <Logo src={logo} />
+
+        <ButtonWithIcon>
+          <RiAddCircleLine size={24} />
+          Doar
+        </ButtonWithIcon>
+      </Header>
+    </Container>
+  )
 }
 
 export default Donations
--- web/src/pages/Donations/styles.ts
+++ web/src/pages/Donations/styles.ts
@@ -1,3 +1,76 @@
 import styled from 'styled-components'
 
-export {}
+import { Link } from 'react-router-dom'
+
+export const Container = styled.div`
+  width: 90vw;
+  max-width: 1200px;
+  min-height: 100vh;
+
+  margin: auto;
+`
+
+export const Header = styled.header`
+  width: 100%;
+
+  padding: 1.6rem 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const BackLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font: 500 1.6rem Montserrat;
+  text-decoration: none;
+
+  color: ${props => props.theme.colors.primary};
+
+  transition: color ${props => props.theme.time.transitionButton};
+
+  &:hover {
+    color: ${props => props.theme.colors.secondary};
+  }
+
+  & svg {
+    margin-right: .4rem;
+  }
+`
+
+export const Logo = styled.img`
+  @media (max-width: 700px) {
+    display: none;
+  }
+
+  height: 3.2rem;
+  object-fit: contain;
+`
+
+export const ButtonWithIcon = styled.button`
+  padding: 1.6rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 0;
+  border-radius: .8rem;
+
+  background: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.text.common.inButton};
+
+  cursor: pointer;
+  transition: background-color ${props => props.theme.time.transitionButton};
+
+  &:hover {
+    background: ${props => props.theme.colors.secondary};
+  }
+
+  & svg {
+    margin-right: .8rem;
+  }
+`
