3ed6b08e594128ae7ad743e813a7edec32b147cb
cristianprochnow <cristianprochnow@gmail.com>
2020-09-03 22:42:47 -0300

    feat(Donations): add categories box into search box

--- web/src/pages/Donations/index.tsx
+++ web/src/pages/Donations/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 import {
-  Container
+  Container,
+  SearchContainer,
+  SubContainer,
+  SubTitle,
+  Card,
+  CardIcon,
+  CardLabel,
+  CardContainer
 } from './styles'
 import ButtonWithIcon from '../../components/ButtonWithIcon'
 import Header from '../../components/Header'
+import Description from '../../components/Description'
 
 import {
   RiAddCircleLine
@@ -19,6 +27,60 @@ const Donations: React.FC = () => {
           Icon={RiAddCircleLine}
         />
       </Header>
+
+      <SearchContainer>
+        <SubContainer>
+          <SubTitle>Categorias</SubTitle>
+          <Description>
+            Selecione as categorias de itens que você deseja.
+          </Description>
+
+          <CardContainer>
+            <Card>
+              <CardIcon
+                src="http://localhost:3333/icons/cloth.svg"
+                alt="Roupas"
+              />
+              <CardLabel>Roupas</CardLabel>
+            </Card>
+            <Card>
+              <CardIcon
+                src="http://localhost:3333/icons/cloth.svg"
+                alt="Roupas"
+              />
+              <CardLabel>Roupas</CardLabel>
+            </Card>
+            <Card>
+              <CardIcon
+                src="http://localhost:3333/icons/cloth.svg"
+                alt="Roupas"
+              />
+              <CardLabel>Roupas</CardLabel>
+            </Card>
+            <Card>
+              <CardIcon
+                src="http://localhost:3333/icons/cloth.svg"
+                alt="Roupas"
+              />
+              <CardLabel>Eletrônicos</CardLabel>
+            </Card>
+            <Card>
+              <CardIcon
+                src="http://localhost:3333/icons/cloth.svg"
+                alt="Roupas"
+              />
+              <CardLabel>Roupas</CardLabel>
+            </Card>
+            <Card>
+              <CardIcon
+                src="http://localhost:3333/icons/cloth.svg"
+                alt="Roupas"
+              />
+              <CardLabel>Roupas</CardLabel>
+            </Card>
+          </CardContainer>
+        </SubContainer>
+      </SearchContainer>
     </Container>
   )
 }
--- web/src/pages/Donations/styles.ts
+++ web/src/pages/Donations/styles.ts
@@ -7,3 +7,60 @@ export const Container = styled.div`
 
   margin: auto;
 `
+
+export const SearchContainer = styled.section`
+  width: 100%;
+  margin-top: .8rem;
+  padding: 2.4rem;
+
+  border: 0;
+  border-radius: 1.6rem;
+
+  background-color: ${props => props.theme.colors.inBackground};
+`
+
+export const SubContainer = styled.div`
+  width: 100%;
+`
+
+export const SubTitle = styled.h2`
+  font: 700 3.2rem Comfortaa;
+  line-height: 4.8rem;
+
+  color: ${props => props.theme.text.title.base};
+`
+
+export const CardContainer = styled.div`
+  width: 100%;
+  margin-top: 1.6rem;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(16rem, 1fr));
+  grid-gap: .8rem;
+`
+
+export const Card = styled.li`
+  padding: .8rem 1.6rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: .4rem solid;
+  border-color: ${props => props.theme.colors.black[200]};
+  border-radius: .8rem;
+  background-color: ${props => props.theme.card.background};
+
+  list-style: none;
+`
+
+export const CardIcon = styled.img`
+  height: 2.4rem;
+  margin-right: .8rem;
+`
+
+export const CardLabel = styled.span`
+  font: 500 1.6rem Montserrat;
+
+  color: ${props => props.theme.text.common.primary};
+`
