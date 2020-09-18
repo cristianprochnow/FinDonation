c69df43099044cae51b5ef84119fa6a3032fb163
Cristian Prochnow <cristianprochnow@gmail.com>
2020-09-18 00:05:56 -0300

    feat(routes): add UserMiddleware for token verification

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,25 @@
 import { Router } from 'express'
 
 import UsersController from '@controllers/UsersController'
+import UsersMiddleware from '@middlewares/UsersMiddleware'
 
 const router = Router()
 
 const usersController = new UsersController()
+const usersMiddleware = new UsersMiddleware()
 
 router.get('/users', usersController.index)
 router.post('/users/login', usersController.logIn)
 router.post('/users/signup', usersController.signUp)
-router.get('/users/profile/:id', usersController.profile)
-router.post('/users/deactivate/:id', usersController.deactivate)
+router.get(
+  '/users/profile/:id',
+  usersMiddleware.token,
+  usersController.profile
+)
+router.post(
+  '/users/deactivate/:id',
+  usersMiddleware.token,
+  usersController.deactivate
+)
 
 export { router }
