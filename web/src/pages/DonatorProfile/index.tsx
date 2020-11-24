ac46b60579fa8e63479ae6f0a7e12430aecb65c0
Cristian Prochnow <cristianprochnow@gmail.com>
2020-11-24 20:54:53 -0300

    feat(web): finish styles of donator's profile

--- web/src/pages/DonatorProfile/index.tsx
+++ web/src/pages/DonatorProfile/index.tsx
@@ -4,6 +4,10 @@ import image from '../../assets/images/image.jpg'
 import './styles.css'
 import Title from '../../components/Title'
 import Description from '../../components/Description'
+import {
+  RiWhatsappLine,
+  RiMailLine
+} from 'react-icons/ri'
 
 const DonatorProfile: React.FC = () => {
   return (
@@ -17,6 +21,17 @@ const DonatorProfile: React.FC = () => {
           <Title>Exército da Salvação</Title>
           <Description>Somos uma ONG que visa em ajudar o próximo e dar-lhe assim itens para auxiliar em sua vivência.</Description>
         </header>
+
+        <footer>
+          <button id="whatsapp">
+            <RiWhatsappLine size={20} />
+            WhatsApp
+          </button>
+          <button id="email">
+            <RiMailLine size={20} />
+            E-mail
+          </button>
+        </footer>
       </main>
     </div>
   )
